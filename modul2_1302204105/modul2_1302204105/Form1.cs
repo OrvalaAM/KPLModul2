5ae33ff403ec01394a94c321a7ece574f58049dd
OrvalaAM <90945844+OrvalaAM@users.noreply.github.com>
2022-03-05 17:45:17 +0700

    menambahkan layuot GUI kalkulator

--- modul2_1302204105/modul2_1302204105/Form1.cs
+++ modul2_1302204105/modul2_1302204105/Form1.cs
@@ -12,9 +12,45 @@ namespace modul2_1302204105
 {
     public partial class Form1 : Form
     {
+        double a = 0;
+        double b = 0;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            if(textBox1.Text == "") { 
+                if(button.Text != "0")
+                {
+                    textBox1.Text = textBox1.Text + button.Text;
+                }
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + button.Text;
+            }
+        }
+
+        private void plus(object sender, EventArgs e)
+        {
+            if(a != 0)
+            {
+                a = Convert.ToDouble(textBox1.Text);
+            }
+            textBox1.Text = "";
+            
+        }
+
+        private void samadengan(object sender, EventArgs e)
+        {
+            b = Convert.ToDouble(textBox1.Text);
+            double hasil = a + b;
+            textBox1.Text = hasil.ToString();
+        }
+
+        
     }
 }
